2c9fb32b17c2d08264864ab80c22335634671171
Ahmad Eka <ahmadekaprasetya05@gmail.com>
2025-05-01 00:29:04 +0700

    almost done

---
+++ js/js/script.js
@@ -0,0 +1,56 @@
+function konversikefahrenheit() {
+    const celsiusInput = document.getElementById("celsiusInput");
+    const fahrenheitResult = document.getElementById("fahrenheitResult");
+    const fahrenheitFormula = document.getElementById("fahrenheitFormula");
+    const celsiusError = document.getElementById("celsiusError");
+  
+    const value = parseFloat(celsiusInput.value);
+
+    // Validasi angka jika kosong
+    if (isNaN(value)) {
+      celsiusError.textContent = "hatimu bole kosong, tapi yang ini gabolee";
+      fahrenheitResult.textContent = "";
+      fahrenheitFormula.textContent = "";
+      return;
+    }
+  
+    celsiusError.textContent = "";
+    const fahrenheit = (value * 9 / 5) + 32;
+    fahrenheitResult.textContent = `${value}°C = ${fahrenheit.toFixed(1)}°F`;
+    // Tampilan perhitungan di konversi celcius ke fahrenheit
+    fahrenheitFormula.textContent = `${value} × (9 / 5) + 32\n= ${fahrenheit.toFixed(1)}°F`;
+  }
+  
+  function konversikecelcius() {
+    const fahrenheitInput = document.getElementById("fahrenheitInput");
+    const celsiusResult = document.getElementById("celsiusResult");
+    const celsiusFormula = document.getElementById("celsiusFormula");
+    const fahrenheitError = document.getElementById("fahrenheitError");
+  
+    const value = parseFloat(fahrenheitInput.value);
+    // Validasi angka jika kosong
+    if (isNaN(value)) {
+      fahrenheitError.textContent = "hatimu bole kosong, tapi yang ini gabolee";
+      celsiusResult.textContent = "";
+      celsiusFormula.textContent = "";
+      return;
+    }
+  
+    fahrenheitError.textContent = "";
+    const celsius = (value - 32) * 5 / 9;
+    celsiusResult.textContent = `${value}°F = ${celsius.toFixed(1)}°C`;
+  
+    // Tampilan perhitungan di konversi fahrenheit ke celcius
+    celsiusFormula.textContent = `(${value} - 32) × (5 / 9)\n= ${value - 32} × 5 / 9\n= ${celsius.toFixed(1)}°C`;
+  }
+  
+  function resetFields() {
+    document.getElementById("celsiusInput").value = "";
+    document.getElementById("fahrenheitInput").value = "";
+    document.getElementById("fahrenheitResult").textContent = "";
+    document.getElementById("celsiusResult").textContent = "";
+    document.getElementById("fahrenheitFormula").textContent = "";
+    document.getElementById("celsiusFormula").textContent = "";
+    document.getElementById("celsiusError").textContent = "";
+    document.getElementById("fahrenheitError").textContent = "";
+  }
